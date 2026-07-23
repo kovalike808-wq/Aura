362185c532a0ebea307c7a844e78cb33d6e40907
aura <kovalike808@gmail.com>
2026-07-23 14:06:22 +0700

    Add files via upload

--- server.ts
+++ server.ts
@@ -867,10 +867,12 @@ async function init() {
     app.use(vite.middlewares);
   } else {
     const distPath = path.join(process.cwd(), 'dist');
-    app.use(express.static(distPath));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
-    });
+    if (fs.existsSync(distPath)) {
+      app.use(express.static(distPath));
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(distPath, 'index.html'));
+      });
+    }
   }
 
   // Test Firestore connection on startup
